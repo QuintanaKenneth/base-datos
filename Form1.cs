179d5a827cc03d02c0aeab1dc4ca3682b696b513
Kenneth <71732110+QuintanaKenneth@users.noreply.github.com>
2020-10-20 22:39:02 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BaseDeDatos603
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MostrarUsuario();
+            string connection = "datasource=localhost;port=3306;username=root;password=;database=proyecto";
+            string query = "SELECT * FROM personas";
+            MySqlConnection conectionDatabase = new MySqlConnection(connection);
+            MySqlCommand databaseCommand = new MySqlCommand(query, conectionDatabase);
+            databaseCommand.CommandTimeout = 60;
+            MySqlDataReader reader;
+            string datos;
+
+
+            try
+            {
+                conectionDatabase.Open();
+                reader = databaseCommand.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+
+                        Console.WriteLine(reader.GetString(0) + " - " + reader.GetString(1) + " - " + reader.GetString(2) + " - " + reader.GetString(3));
+                        datos = reader.GetString(0) + " - " + reader.GetString(1) + " - " + reader.GetString(2) + " - " + reader.GetString(3);
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ningun dato existente mi carnal");
+                }
+                conectionDatabase.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Eliminar()
+        {
+            string connection = "datasource=localhost;port=3306;username=root;password=;database=proyecto";
+            string query = "DELETE  FROM `personas` WHERE ID  =" + textBox4.Text;
+            MySqlConnection conectionDatabase = new MySqlConnection(connection);
+            MySqlCommand databaseCommand = new MySqlCommand(query, conectionDatabase);
+            databaseCommand.CommandTimeout = 60;
+
+            try
+            {
+                conectionDatabase.Open();
+                MySqlDataReader myeader1 = databaseCommand.ExecuteReader();
+                MessageBox.Show("Esta borrado el dato, como te borro tu ex de Fb");
+                MostrarUsuario();
+                conectionDatabase.Close();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void Actualizar()
+        {
+            string connection = "datasource=localhost;port=3306;username=root;password=;database=proyecto;";
+            string query = "UPDATE `personas` SET `NAME`='" + textBox1.Text + "',`LAST NAME`='" + textBox3.Text + "',`ADDRESS`='" + textBox2.Text + "' WHERE ID = '" + textBox4.Text + "' ";
+            MySqlConnection conectionDatabase = new MySqlConnection(connection);
+            MySqlCommand databaseCommand = new MySqlCommand(query, conectionDatabase);
+            databaseCommand.CommandTimeout = 60;
+
+            try
+            {
+                conectionDatabase.Open();
+                MySqlDataReader myreader1 = databaseCommand.ExecuteReader();
+                MessageBox.Show("Ulala ya se actualizó el dato mijo");
+                conectionDatabase.Close();
+                textBox1.Text = "";
+                textBox3.Text = "";
+                textBox2.Text = "";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void Buscar()
+        {
+            string connection = "datasource=localhost;port=3306;username=root;password=;database=proyecto;";
+            string query = "SELECT * FROM personas where `ID`= '" + textBox4.Text + "' ";
+            MySqlConnection conectionDatabase = new MySqlConnection(connection);
+            MySqlCommand databaseCommand = new MySqlCommand(query, conectionDatabase);
+            databaseCommand.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+
+            try
+            {
+                conectionDatabase.Open();
+                reader = databaseCommand.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    listView1.Items.Clear();
+                    while (reader.Read())
+                    {
+                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3) };
+
+                        textBox1.Text = row[1];
+                        textBox3.Text = row[2];
+                        textBox2.Text = row[3];
+
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("NO HAY NADA, NO SE ENCONTRO PA");
+                }
+                conectionDatabase.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void GuardarUsuario()
+        {
+            string connection = "datasource=localhost;port=3306;username=root;password=;database=proyecto";
+            string query = "INSERT INTO personas(`ID`, `NAME`, `LAST NAME`, `ADDRESS`) VALUES (NULL, '" + textBox1.Text + "', '" + textBox3.Text + "', '" + textBox2.Text + "')";
+            MySqlConnection conectionDatabase = new MySqlConnection(connection);
+            MySqlCommand databaseCommand = new MySqlCommand(query, conectionDatabase);
+            databaseCommand.CommandTimeout = 60;
+
+            try
+            {
+                conectionDatabase.Open();
+                MySqlDataReader myreader1 = databaseCommand.ExecuteReader();
+                MessageBox.Show("Se registro con éxito mama, pura fabrica de muñecos");
+                conectionDatabase.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+
+        private void MostrarUsuario()
+        {
+            string Connect = "datasource=localhost;port=3306;username=root;password=;database=proyecto;";
+            string query = "SELECT * FROM personas";
+            MySqlConnection databaseConnection = new MySqlConnection(Connect);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        listView1.Items.Clear();
+                        while (reader.Read())
+                        {
+                            string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3) };
+                            var ListViewItems = new ListViewItem(row);
+                            listView1.Items.Add(ListViewItems);
+                        }
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("NO HAY NO EXISTE, NO FUE ENCONTRADO, COMO TU GFE");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("NO HAY NOMBRE CARNALITO, NO SON ENCHILADAS EH");
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("NO TIENES DIRECCIÓN O QUE? ANOTALA PLOX");
+            }
+            else if (textBox3.Text == "")
+            {
+                MessageBox.Show("NO TIENES APELLIDO?, CRECISTE DE LA TIERRA O COMO?");
+            }
+            else
+            {
+
+                GuardarUsuario();
+                MostrarUsuario();
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//Actualizar
+            MostrarUsuario();
+
+        }
+
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Buscar();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Actualizar();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Eliminar();
+        }
+    }
+}
